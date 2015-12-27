7b75aa3e3b5d2212a3cd0f7dca99bf067ba7255e
yoodaa <dino.onex@gmail.com>
2015-12-27 14:15:16 +0100

    Basic functionality, no tests yet

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sing_up: 'register' }
+  resources :movies, only: [:show, :index]
+  root 'movies#index'
+
+end
